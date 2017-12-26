994db0c2369332eefbc0cd35c2fcdcfa7724055b
Asif Al Noman <asifnoman240@gmail.com>
2017-12-26 18:56:26 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ Team Members =>
    Raihan Faisal -> 14-27108-2 [F]
    Ruman, Md Saifuddin -> 14-27039-3 [F]
 
+   Github link - github.com/AsifNoman/Village-Racing
+
 */
 
 #include <stdio.h>
@@ -25,9 +27,10 @@ int height = 100, width = 100 ;
 int window_width = 620, window_height = 480;
 int window_width2 = 300, window_height2 = 480;
 int mx = window_width/4 - width/4, my = 10;
-int b1x = rand() % window_width2 + 130, b1y = window_height;
+int b1x = rand() % window_width2 + 200, b1y = window_height;
 int b2x = rand() % window_width + 1, b2y = window_height;
-int c1x = rand() % window_width2 + 110, c1y = window_height;
+int c1x = rand() % window_width2 + 190, c1y = window_height;
+
 float point = 0.0;
 
 int windowFlag=0;
@@ -35,6 +38,14 @@ int windowFlag=0;
 int boatX1, boatY1;
 int bushX1, bushY1, bushX2, bushY2;
 int coinX1, coinY1;
+
+int b3x = rand() % window_width2 + 200, b3y = window_height;
+int c3x = rand() % window_width2 + 190, c3y = window_height;
+int bushX3, bushY3, bushX4, bushY4;
+int coinX2, coinY2;
+
+int b4x = rand() % window_width2 + 200, b4y = window_height;
+int bushX5, bushY5, bushX6, bushY6;
 
 void myInit (void)
 {
@@ -504,6 +515,29 @@ void countTime()
     }
 }
 
+//////////////////////////////////////////////////////////////
+
+
+void keyboardAction(int key, int x, int y)
+{
+    switch(key)
+    {
+        case GLUT_KEY_RIGHT:
+             mx += 10;
+            glutPostRedisplay();
+            break;
+
+        case GLUT_KEY_LEFT:
+             mx -= 10;
+            glutPostRedisplay();
+            break;
+
+        default:
+            break;
+    }
+}
+
+//////////////////////////////////////////////////////////////
 
 void drawScore (void * font, char *s, float x, float y){
      glRasterPos2f(x, y);
@@ -545,16 +579,39 @@ void translate()
     b1y -= 10;
     c1y -= 10;
 
+    b3y -= 10;
+    c3y -= 10;
+
+    b4y -= 10;
+
     if(b1y <= -80 )
     {
-       b1x = rand() % window_width2 + 115 ;
+       b1x = rand() % (window_width2-15) + 195 ;
        b1y = rand() % (window_height2*2 ) + window_height2;
     }
 
     if(c1y <= -80 )
     {
-        c1x = rand() % window_width2 + 115 ;
+        c1x = rand() % (window_width2-15) + 195 ;
         c1y = rand() % (window_height2*2 ) + window_height2;
+    }
+
+    if(b3y <= -80 )
+    {
+       b3x = rand() % (window_width2-20) + 195 ;
+       b3y = rand() % (window_height2*2 ) + window_height2;
+    }
+
+    if(c3y <= -80 )
+    {
+        c3x = rand() % (window_width2-20) + 195 ;
+        c3y = rand() % (window_height2*2 ) + window_height2;
+    }
+
+    if(b4y <= -80 )
+    {
+       b4x = rand() % (window_width2-20) + 195 ;
+       b4y = rand() % (window_height2*2 ) + window_height2;
     }
 
     // right side boundary
@@ -581,7 +638,17 @@ void translate()
         lose+=0.14;
     }
 
-    Sleep(100);
+    if ( boatY1>=bushY3 && boatX1+13>=bushX3 && boatX1-13<=bushX4 )
+    {
+        lose+=0.14;
+    }
+
+    if ( boatY1>=bushY5 && boatX1+13>=bushX5 && boatX1-13<=bushX6 )
+    {
+        lose+=0.14;
+    }
+
+    Sleep(35);
 
    glutPostRedisplay();
 
@@ -677,6 +744,31 @@ glVertex2i(102,500);
 
 glEnd();
 //glFlush ();
+
+glBegin(GL_QUAD_STRIP);
+
+
+glColor3ub(222.0,184.0,135.0);
+glVertex2i(555,0);
+glVertex2i(562,0);
+glVertex2i(562,500);
+glVertex2i(555,500);
+//glFlush ();
+
+glColor3ub (244.0,164.0,96.0);
+glVertex2i(557,0);
+glVertex2i(582,0);
+glVertex2i(557,500);
+glVertex2i(582,500);
+//glFlush ();
+
+glColor3ub(222.0,184.0,135.0);
+glVertex2i(580,0);
+glVertex2i(587,0);
+glVertex2i(587,500);
+glVertex2i(580,500);
+
+glEnd();
 
 
 }
@@ -1217,6 +1309,345 @@ glEnd();
 glFlush ();
 
 
+//////////////////////////////////////////////////////////////////////////
+
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+
+glVertex2i(537, bsy+60);
+glVertex2i(550, bsy+65);
+glVertex2i(550, bsy+71);
+
+glVertex2i(550, bsy+65);
+glVertex2i(550, bsy+75);
+glVertex2i(528, bsy+70);
+
+glVertex2i(550, bsy+72);
+glVertex2i(550, bsy+80);
+glVertex2i(518, bsy+78);
+
+glVertex2i(550, bsy+77);
+glVertex2i(550, bsy+88);
+glVertex2i(528, bsy+85);
+
+glVertex2i(533, bsy+96);
+glVertex2i(550, bsy+90);
+glVertex2i(550, bsy+80);
+
+glVertex2i(550,bsy+87);
+glVertex2i(550,bsy+92);
+glVertex2i(543,bsy+98);
+
+glVertex2i(550,bsy+62);
+glVertex2i(550,bsy+67);
+glVertex2i(543,bsy+58);
+glEnd();
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+
+glVertex2i(533, bsy+160);
+glVertex2i(550, bsy+165);
+glVertex2i(550, bsy+171);
+
+glVertex2i(550, bsy+165);
+glVertex2i(550, bsy+175);
+glVertex2i(528, bsy+170);
+
+glVertex2i(550, bsy+172);
+glVertex2i(550, bsy+180);
+glVertex2i(518, bsy+178);
+
+glVertex2i(550, bsy+177);
+glVertex2i(550, bsy+188);
+glVertex2i(528, bsy+185);
+
+glVertex2i(533, bsy+196);
+glVertex2i(550, bsy+190);
+glVertex2i(550, bsy+180);
+
+glVertex2i(550,bsy+187);
+glVertex2i(550,bsy+192);
+glVertex2i(543,bsy+198);
+
+glVertex2i(550,bsy+162);
+glVertex2i(550,bsy+167);
+glVertex2i(543,bsy+158);
+
+glEnd();
+
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+
+glVertex2i(533, bsy+260);
+glVertex2i(550, bsy+265);
+glVertex2i(550, bsy+271);
+
+glVertex2i(550, bsy+265);
+glVertex2i(550, bsy+275);
+glVertex2i(528, bsy+270);
+
+glVertex2i(550, bsy+272);
+glVertex2i(550, bsy+280);
+glVertex2i(518, bsy+278);
+
+glVertex2i(550, bsy+277);
+glVertex2i(550, bsy+288);
+glVertex2i(528, bsy+285);
+
+glVertex2i(533, bsy+296);
+glVertex2i(550, bsy+290);
+glVertex2i(550, bsy+280);
+
+glVertex2i(550,bsy+287);
+glVertex2i(550,bsy+292);
+glVertex2i(543,bsy+298);
+
+glVertex2i(550,bsy+262);
+glVertex2i(550,bsy+267);
+glVertex2i(543,bsy+258);
+
+glEnd();
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+
+glVertex2i(533, bsy+360);
+glVertex2i(550, bsy+365);
+glVertex2i(550, bsy+371);
+
+glVertex2i(550, bsy+365);
+glVertex2i(550, bsy+375);
+glVertex2i(528, bsy+370);
+
+glVertex2i(550, bsy+372);
+glVertex2i(550, bsy+380);
+glVertex2i(518, bsy+378);
+
+glVertex2i(550, bsy+377);
+glVertex2i(550, bsy+388);
+glVertex2i(528, bsy+385);
+
+glVertex2i(533, bsy+396);
+glVertex2i(550, bsy+390);
+glVertex2i(550, bsy+380);
+
+glVertex2i(550,bsy+387);
+glVertex2i(550,bsy+392);
+glVertex2i(543,bsy+398);
+
+glVertex2i(550,bsy+362);
+glVertex2i(550,bsy+367);
+glVertex2i(543,bsy+358);
+glEnd();
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+
+glVertex2i(533, bsy+460);
+glVertex2i(550, bsy+465);
+glVertex2i(550, bsy+471);
+
+glVertex2i(550, bsy+465);
+glVertex2i(550, bsy+475);
+glVertex2i(528, bsy+470);
+
+glVertex2i(550, bsy+472);
+glVertex2i(550, bsy+480);
+glVertex2i(518, bsy+478);
+
+glVertex2i(550, bsy+477);
+glVertex2i(550, bsy+488);
+glVertex2i(528, bsy+485);
+
+glVertex2i(533, bsy+496);
+glVertex2i(550, bsy+490);
+glVertex2i(550, bsy+480);
+
+glVertex2i(550,bsy+487);
+glVertex2i(550,bsy+492);
+glVertex2i(543,bsy+498);
+
+glVertex2i(550,bsy+462);
+glVertex2i(550,bsy+467);
+glVertex2i(543,bsy+458);
+
+glEnd();
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+
+glVertex2i(603, bsy+20);
+glVertex2i(586, bsy+25);
+glVertex2i(586, bsy+31);
+
+glVertex2i(586, bsy+25);
+glVertex2i(586, bsy+35);
+glVertex2i(608, bsy+30);
+
+glVertex2i(586, bsy+32);
+glVertex2i(586, bsy+40);
+glVertex2i(618, bsy+38);
+
+glVertex2i(586, bsy+37);
+glVertex2i(586, bsy+48);
+glVertex2i(618, bsy+45);
+
+glVertex2i(603, bsy+56);
+glVertex2i(586, bsy+50);
+glVertex2i(586, bsy+40);
+
+glVertex2i(586,bsy+47);
+glVertex2i(586,bsy+52);
+glVertex2i(593,bsy+58);
+
+glVertex2i(586,bsy+22);
+glVertex2i(586,bsy+27);
+glVertex2i(593,bsy+18);
+
+glEnd();
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+
+glVertex2i(603, bsy+120);
+glVertex2i(586, bsy+125);
+glVertex2i(586, bsy+131);
+
+glVertex2i(586, bsy+125);
+glVertex2i(586, bsy+135);
+glVertex2i(608, bsy+130);
+
+glVertex2i(586, bsy+132);
+glVertex2i(586, bsy+140);
+glVertex2i(618, bsy+138);
+
+glVertex2i(586, bsy+137);
+glVertex2i(586, bsy+148);
+glVertex2i(618, bsy+145);
+
+glVertex2i(603, bsy+156);
+glVertex2i(586, bsy+150);
+glVertex2i(586, bsy+140);
+
+glVertex2i(586,bsy+147);
+glVertex2i(586,bsy+152);
+glVertex2i(603,bsy+158);
+
+glVertex2i(586,bsy+122);
+glVertex2i(586,bsy+127);
+glVertex2i(593,bsy+118);
+
+glEnd();
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+
+glVertex2i(603, bsy+220);
+glVertex2i(586, bsy+225);
+glVertex2i(586, bsy+231);
+
+glVertex2i(586, bsy+225);
+glVertex2i(586, bsy+235);
+glVertex2i(608, bsy+230);
+
+glVertex2i(586, bsy+232);
+glVertex2i(586, bsy+240);
+glVertex2i(618, bsy+238);
+
+glVertex2i(586, bsy+237);
+glVertex2i(586, bsy+248);
+glVertex2i(618, bsy+245);
+
+glVertex2i(603, bsy+256);
+glVertex2i(586, bsy+250);
+glVertex2i(586, bsy+240);
+
+glVertex2i(586,bsy+247);
+glVertex2i(586,bsy+252);
+glVertex2i(593,bsy+258);
+
+glVertex2i(586,bsy+222);
+glVertex2i(586,bsy+227);
+glVertex2i(593,bsy+218);
+
+glEnd();
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+
+glVertex2i(603, bsy+320);
+glVertex2i(586, bsy+325);
+glVertex2i(586, bsy+331);
+
+glVertex2i(586, bsy+325);
+glVertex2i(586, bsy+335);
+glVertex2i(608, bsy+330);
+
+glVertex2i(586, bsy+332);
+glVertex2i(586, bsy+340);
+glVertex2i(618, bsy+338);
+
+glVertex2i(586, bsy+337);
+glVertex2i(586, bsy+348);
+glVertex2i(618, bsy+345);
+
+glVertex2i(603, bsy+356);
+glVertex2i(586, bsy+350);
+glVertex2i(586, bsy+340);
+
+glVertex2i(586,bsy+347);
+glVertex2i(586,bsy+352);
+glVertex2i(593,bsy+358);
+
+glVertex2i(586,bsy+322);
+glVertex2i(586,bsy+327);
+glVertex2i(593,bsy+318);
+
+
+glEnd();
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+
+glVertex2i(603, bsy+420);
+glVertex2i(586, bsy+425);
+glVertex2i(586, bsy+431);
+
+glVertex2i(586, bsy+425);
+glVertex2i(586, bsy+435);
+glVertex2i(608, bsy+430);
+
+glVertex2i(586, bsy+432);
+glVertex2i(586, bsy+440);
+glVertex2i(618, bsy+438);
+
+glVertex2i(586, bsy+437);
+glVertex2i(586, bsy+448);
+glVertex2i(618, bsy+445);
+
+glVertex2i(603, bsy+456);
+glVertex2i(586, bsy+450);
+glVertex2i(586, bsy+440);
+
+glVertex2i(586,bsy+447);
+glVertex2i(586,bsy+452);
+glVertex2i(593,bsy+458);
+
+glVertex2i(586,bsy+422);
+glVertex2i(586,bsy+427);
+glVertex2i(593,bsy+418);
+glEnd();
+glFlush ();
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
+
 
 
 bushTranslate();
@@ -1329,6 +1760,73 @@ glVertex2i(bsx+13, bsy+55);
 glVertex2i(bushX1=bsx+7, bushY1=bsy+55);  //left most value
 glEnd();
 //glFlush ();
+
+}
+
+void Bush2()
+{
+    int bsx=b3x;
+    int bsy=b3y;
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+glVertex2i(bsx+45, bsy+71);
+glVertex2i(bushX4=bsx+40, bushY4=bsy+55);  // right most value
+glVertex2i(bsx+34, bsy+55);
+glVertex2i(bsx+40, bsy+81);
+glVertex2i(bsx+40, bsy+55);
+glVertex2i(bsx+30, bsy+55);
+glVertex2i(bsx+31, bsy+88);
+glVertex2i(bsx+36, bsy+55);
+glVertex2i(bsx+26, bsy+55);
+glVertex2i(bsx+24, bsy+84);
+glVertex2i(bsx+31, bsy+55);
+glVertex2i(bsx+19, bsy+55);
+glVertex2i(bsx+17, bsy+92);
+glVertex2i(bsx+22, bsy+55);
+glVertex2i(bsx+12, bsy+55);
+glVertex2i(bsx+9, bsy+80);
+glVertex2i(bsx+20, bsy+55);
+glVertex2i(bsx+8, bsy+55);
+glVertex2i(bsx+2, bsy+73);
+glVertex2i(bsx+13, bsy+55);
+glVertex2i(bushX3=bsx+7, bushY3=bsy+55);  //left most value
+glEnd();
+//glFlush ();
+
+}
+
+void Bush3()
+{
+    int bsx=b4x;
+    int bsy=b4y;
+
+glBegin(GL_TRIANGLES);
+glColor3ub(0, 255, 0);
+glVertex2i(bsx+45, bsy+71);
+glVertex2i(bushX5=bsx+40, bushY5=bsy+55);  // right most value
+glVertex2i(bsx+34, bsy+55);
+glVertex2i(bsx+40, bsy+81);
+glVertex2i(bsx+40, bsy+55);
+glVertex2i(bsx+30, bsy+55);
+glVertex2i(bsx+31, bsy+88);
+glVertex2i(bsx+36, bsy+55);
+glVertex2i(bsx+26, bsy+55);
+glVertex2i(bsx+24, bsy+84);
+glVertex2i(bsx+31, bsy+55);
+glVertex2i(bsx+19, bsy+55);
+glVertex2i(bsx+17, bsy+92);
+glVertex2i(bsx+22, bsy+55);
+glVertex2i(bsx+12, bsy+55);
+glVertex2i(bsx+9, bsy+80);
+glVertex2i(bsx+20, bsy+55);
+glVertex2i(bsx+8, bsy+55);
+glVertex2i(bsx+2, bsy+73);
+glVertex2i(bsx+13, bsy+55);
+glVertex2i(bushX6=bsx+7, bushY6=bsy+55);  //left most value
+glEnd();
+//glFlush ();
+
 }
 
 void myDisplay()
@@ -1341,6 +1839,8 @@ void myDisplay()
        myBackground();
        Road();
        Bush();
+       Bush2();
+       Bush3();
        roadSideBush();
        Coin();
        Boat();
@@ -1375,11 +1875,10 @@ int main(int argc, char** argv)
     glutDisplayFunc(myDisplay);
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
+    glutSpecialFunc(keyboardAction);
     myInit ();
     srand(clock());
     glutMainLoop();
 
     return 0;
 }
-
-
